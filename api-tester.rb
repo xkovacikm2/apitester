a616608c59f86b4991976fef4a05d267a69f59b0
Michal Kováčik <michal.kovacik@siemens-healthineers.com>
2020-08-24 15:55:45 +0200

    Add skeleton for forwarding

--- api-tester.rb
+++ api-tester.rb
@@ -1,5 +1,11 @@
+# This project is libre, and licenced under the terms of the
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+# as published by dtf on July 2019. See the COPYING file or
+# https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
+
 require 'sinatra'
 require 'json'
+require 'haml'
 
 set :environment, :production
 set :port, 4567
@@ -35,9 +41,7 @@ get '/flush' do
 end
 
 get '/get_results' do
-  "<pre>
-  #{JSON.pretty_generate storage}
-  </pre>"
+  haml :results, format: :html5, locals: {storage: storage.map{ |stored| JSON.pretty_generate stored} }
 end
 
 def process_body(body)
